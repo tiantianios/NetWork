d577489c9d1b1e54f4523af69f38a34fc022b012
tianxiupingios <tianxiupingios@126.com>
2017-11-01 19:00:06 +0800

    Initial Commit

---
+++ NetWork/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  NetWork
+//
+//  Created by tianxiuping on 2017/11/1.
+//  Copyright © 2017年 TXP. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
